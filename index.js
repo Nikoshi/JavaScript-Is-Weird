5a18100d6bc39d9ecd12527f8922f50ec7a4f472
Nikolas S. Logemann <nikolasl@outlook.de>
2021-02-07 13:59:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const one = '+!![]';
 
 const number = n => {
   if (n === 0) return zero;
-  return Array.from({length: n}, () => one).join(' + ');
+  return Array.from({length: n}, () => one).join('+');
 }
 
 // C
